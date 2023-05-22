ef5db1f0aed034f875ac9e6b8776f1221d5fdefc
Burak Öztürk <oztburak240@gmail.com>
2023-05-22 21:36:39 +0300

    Kayıt Formu Düzenlendi

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import userService from '../services/userService';
+import { Form, Button } from 'semantic-ui-react'
 
 export default function RegisterForm() {
   const [name, setName] = useState('');
@@ -18,30 +19,31 @@ export default function RegisterForm() {
     try {
       await userService.registerUser(newUser);
       console.log('Kullanıcı başarıyla kaydedildi.');
+      alert('Kullanıcı başarıyla kaydedildi.');
       // Başarılı kayıt işlemi tamamlandıktan sonra yapılacak işlemleri buraya ekleyebilirsiniz.
     } catch (error) {
       console.error('Kullanıcı kaydedilirken bir hata oluştu:', error);
     }
   };
-
   return (
     <div>
       <h2>Kayıt Ol</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
+      <Form onSubmit={handleSubmit}>
+        <Form.Field>
           <label>Ad:</label>
           <input type="text" value={name} onChange={(e) => setName(e.target.value)} required />
-        </div>
-        <div>
+        </Form.Field>
+        <Form.Field>
           <label>E-posta:</label>
           <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-        </div>
-        <div>
+        </Form.Field>
+        <Form.Field>
           <label>Şifre:</label>
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-        </div>
-        <button type="submit">Kaydet</button>
-      </form>
+        </Form.Field>
+        <Button type="submit">Kaydet</Button>
+      </Form>
     </div>
   );
+  
 }
