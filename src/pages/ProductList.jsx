ceae7f2569c991a4b0195d74ba33208b2b68d8c9
Burak Öztürk <oztburak240@gmail.com>
2023-05-23 18:18:19 +0300

    ProductList İçin Son Düzenleme

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { Icon, Menu, Table, Button } from 'semantic-ui-react'
+import { Icon, Menu, Table, Button, Image } from 'semantic-ui-react'
 import ProductService from '../services/productService'
 import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { AddToCart } from '../store/actions/cartActions'
-
 
 export default function ProductList() {
     const dispatch = useDispatch()
@@ -15,10 +14,9 @@ export default function ProductList() {
         productService.getProducts().then(result => setProducts(result.data))
     }, [])
 
-
     const handleAddToCart = (product) => {
         console.log("Ürün sepete eklendi:", product);
-        dispatch(AddToCart({ product: product, quantity: 1 })); // Quantity değerini ekleyin
+        dispatch(AddToCart({ product: product, quantity: 1 }));
     }
 
     return (
@@ -26,8 +24,9 @@ export default function ProductList() {
             <Table celled>
                 <Table.Header>
                     <Table.Row>
-
+                        <Table.HeaderCell>Fotoğraf</Table.HeaderCell>
                         <Table.HeaderCell>Ürün Adı</Table.HeaderCell>
+                        <Table.HeaderCell>Ürün Fiyatı</Table.HeaderCell>
                         <Table.HeaderCell>Ürün Sahibi</Table.HeaderCell>
                         <Table.HeaderCell>Ürün Kategori</Table.HeaderCell>
                         <Table.HeaderCell></Table.HeaderCell>
@@ -37,7 +36,13 @@ export default function ProductList() {
                 <Table.Body>
                     {products?.length && products.map((product) => (
                         <Table.Row key={product.id}>
-                            <Table.Cell><Link to={`/products/${product.id}`}>{product.name}</Link></Table.Cell>
+                            <Table.Cell>
+                                <Image src={product.photoUrl} size="tiny" rounded />
+                            </Table.Cell>
+                            <Table.Cell>
+                                <Link to={`/products/${product.id}`}>{product.name}</Link>
+                            </Table.Cell>
+                            <Table.Cell>{product.price}</Table.Cell>
                             <Table.Cell>{product.userName}</Table.Cell>
                             <Table.Cell>{product.categoryName}</Table.Cell>
                             <Table.Cell>
@@ -47,24 +52,6 @@ export default function ProductList() {
                     ))}
                 </Table.Body>
 
-                <Table.Footer>
-                    <Table.Row>
-                        <Table.HeaderCell colSpan='3'>
-                            <Menu floated='right' pagination>
-                                <Menu.Item as='a' icon>
-                                    <Icon name='chevron left' />
-                                </Menu.Item>
-                                <Menu.Item as='a'>1</Menu.Item>
-                                <Menu.Item as='a'>2</Menu.Item>
-                                <Menu.Item as='a'>3</Menu.Item>
-                                <Menu.Item as='a'>4</Menu.Item>
-                                <Menu.Item as='a' icon>
-                                    <Icon name='chevron right' />
-                                </Menu.Item>
-                            </Menu>
-                        </Table.HeaderCell>
-                    </Table.Row>
-                </Table.Footer>
             </Table>
         </div>
     )
